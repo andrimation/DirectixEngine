2f422a286e980fe4a52f567d1c847f0a425d4469
andrimation <andrimation@gmail.com>
2023-12-23 12:51:40 +0100

    first

---
+++ Window.cpp
@@ -0,0 +1,11 @@
+#include "Window.h"
+
+bool Window::init()
+{
+	return false;
+}
+
+bool Window::release()
+{
+	return false;
+}
---
+++ Window.h
@@ -0,0 +1,12 @@
+#pragma once
+class Window
+{
+	Window();
+
+	bool init();
+
+	bool release();
+
+	~Window();
+};
+
